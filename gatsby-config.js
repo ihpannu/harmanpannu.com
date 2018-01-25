9e216abe75ba702b31c99d13a627bd10c6e2ab9b
iharmanpannu <iharmanpannu@gmail.com>
2018-01-25 11:43:10 -0800

    added offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: "Harman Pannu"
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet", 
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/favicon.png",
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: true,
-          yandex: true,
-          windows: true
-        }
-      }
+    siteMetadata: {
+        title: "Harman Pannu"
     },
-  ]
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: true,
+                    yandex: true,
+                    windows: true
+                }
+            }
+        },
+    ]
 };
