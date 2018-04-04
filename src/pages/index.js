eee80e241a1a730ff33187a82cad6937d9014595
Harman Pannu <iharmanpannu@gmail.com>
2018-04-03 23:51:17 -0700

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,7 @@ import Home from '../components/Home/Home'
 
 const IndexPage = () => (
   <div>
-    <Home/> {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link> */}
+    <Home/>
   </div>
 )
 
